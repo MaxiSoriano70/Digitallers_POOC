c2c4089896a79f6ff4d43b1d2e50507f0f6dd09b
Maxi Soriano <69224410+MaxiSoriano70@users.noreply.github.com>
2021-08-12 03:30:37 -0300

    Clase 2 OOP

---
+++ oop/Empleado.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    class Empleado
+    {
+        public string Nombre { get; set; }
+        public int Legajo { get; set; }
+        //public double Sueldo { get; set; }
+        protected double Sueldo;
+        public void Trabajar()
+        {
+            Console.WriteLine("Estoy trababjando como un empleado");
+        }
+        public virtual void setSueldo(double s)
+        {
+            Sueldo = s;
+        }
+        public double getSueldo()
+        {
+            return Sueldo;
+        }
+    }
+}
