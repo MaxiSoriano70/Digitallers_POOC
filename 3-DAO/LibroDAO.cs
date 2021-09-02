306d21c198c6deb4d26643120eed000acaf6be26
Maxi Soriano <69224410+MaxiSoriano70@users.noreply.github.com>
2021-09-01 22:06:47 -0300

    Clase 1/9/2021

--- 3-DAO/LibroDAO.cs
+++ 3-DAO/LibroDAO.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using Entities;
 
 namespace _3_DAO
@@ -15,6 +16,27 @@ namespace _3_DAO
             libro.Titulo = "Test Libro";
             libro.Editorial = "Test Editorial";
             return libro;
+        }
+        public static List<Libro> GetLibros(string connectionString)
+        {
+            List<Libro> libros = new List<Libro>();
+            using (SqlConnection con=new SqlConnection(connectionString))
+            {
+                string query = "select * from consulta_libros";
+                SqlCommand cmd = new SqlCommand(query, con);
+                con.Open();
+                SqlDataReader rd = cmd.ExecuteReader();
+                while (rd.Read())
+                {
+                    Libro libro=new Libro();
+                    libro.Id = Convert.ToInt32(rd["id_libro"]);
+                    libro.Titulo = rd["titulo"].ToString();
+                    libro.Autor = rd["autor"].ToString();
+                    libro.Editorial = rd["editorial"].ToString();
+                    libros.Add(libro);
+                }
+                /*Aca adentro va a existir con*/
+            }
         }
     }
 }
--- Entities/Libro.cs
+++ Entities/Libro.cs
@@ -9,7 +9,9 @@ namespace Entities
 {
     public class Libro
     {
+        public int Id { get; set; }
         public string Titulo { get; set; }
         public string Editorial { get; set; }
+        public string Autor { get; set; }
     }
 }
