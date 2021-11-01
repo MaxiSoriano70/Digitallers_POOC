572d85a0ec00b91b40b6bd1d8824151e15ad9883
Maxi Soriano <69224410+MaxiSoriano70@users.noreply.github.com>
2021-11-01 02:25:36 -0300

    EntitiesII

---
+++ Operaciones.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Entity2
+{
+    public delegate void MensajeError(string mensaje);
+    public delegate void MostrarTituloMensaje();
+    public delegate bool TestLoco();
+    public class Operaciones
+    {
+        public MensajeError WriteError;
+        public MostrarTituloMensaje TituloMsg;
+        public int Sumar(int Num1, int Num2)
+        {
+            int Suma = Num1 + Num2;
+            if(Suma < 0)
+            {
+                //throw new Exception("La suma dio negativo");
+                TituloMsg();
+                WriteError("La suma dio negativo");
+
+            }
+            return Suma;
+        }
+
+        public int Restar(int Num1, int Num2)
+        {
+            int Resta = Num1 - Num2;
+            if (Resta < 0)
+            {
+                WriteError("La resta dio negativo");
+
+            }
+            return Resta;
+        }
+
+        public int Multiplicar(int Num1,int Num2, Action<string> EjemploAction)
+        {
+            int Multi = Num1 * Num2;
+            if(Multi == 100)
+            {
+                EjemploAction("La multiplicación me devolvió 100 y me da ansiedad");
+            }
+            return Multi;
+        }
+
+        public string SumaFuncOp(int Num1, int Num2, Func<int,int,string> EjemploFunc)
+        {
+            string resultado = EjemploFunc(Num1, Num2);
+            return resultado;
+        }
+
+    }
+}
---
+++ RefOperaciones.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+
+namespace Entity2
+{
+    public class RefOperaciones
+    {
+        public int EjemploDelegado(int Num1, int Num2)
+        {
+            Operaciones op = new Operaciones();
+            op.WriteError = MostrarMensaje;
+            op.TituloMsg = MostrarLinea;
+            return op.Sumar(Num1, Num2);
+        }
+
+        public int EjemploAnonimo(int Num1, int Num2)
+        {
+            Operaciones op = new Operaciones();
+            op.WriteError = delegate (string mensaje)
+            {
+                Debug.WriteLine("Mensaje a través de un método anónimo: " + mensaje);
+            };
+
+            return op.Restar(Num1, Num2);
+        }
+
+        public int EjemploLambda(int Num1, int Num2)
+        {
+            Operaciones op = new Operaciones();
+            /*op.WriteError = delegate (string mensaje)
+            {
+                Debug.WriteLine("Mensaje a través de un método anónimo: " + mensaje);
+            };*/
+
+            op.WriteError = mensaje => Debug.WriteLine("Mensaje a través de una Lambda Expression: " + mensaje);
+
+            return op.Restar(Num1, Num2);
+        }
+
+        public int EjemploAction(int Num1, int Num2)
+        {
+            Operaciones op = new Operaciones();
+            Action<string> EjAction;
+            EjAction = MostrarMensaje;
+            return op.Multiplicar(Num1, Num2, EjAction);
+        }
+
+        public string EjemploFunc(int Num1,int Num2)
+        {
+            Operaciones op = new Operaciones();
+            Func<int, int, string> EjFunc;
+            EjFunc = SumaFunc;
+            return op.SumaFuncOp(Num1,Num2,EjFunc);
+        }
+
+        public string SumaFunc(int Num1,int Num2)
+        {
+            int Suma = Num1 + Num2;
+            return "La suma de los números es " + Suma;
+        }
+
+
+        public void MostrarMensaje(string mensaje)
+        {
+            Debug.WriteLine(mensaje);
+        }
+
+        public void MostrarLinea()
+        {
+            Debug.WriteLine("--------------- ERROR ---------------");
+        }
+
+        
+
+        public bool Test()
+        {
+            return true;
+        }
+    }
+}
